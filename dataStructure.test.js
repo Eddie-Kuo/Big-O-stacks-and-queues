9a6ca351702eb60e59ceb564e469983c118cbd88
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-01-26 17:33:48 -0600

    CH-02 + test

--- dataStructure.test.js
+++ dataStructure.test.js
@@ -1,7 +1,8 @@
 const { Stack } = require('./CH-01.js');
+const { Queue } = require('./CH-02');
 
 describe('CH-01', () => {
-  it('add item to list', () => {
+  test('add item to list', () => {
     const newStack = new Stack();
     newStack.push(1);
     newStack.push(1);
@@ -9,15 +10,37 @@ describe('CH-01', () => {
     expect(newStack.readableList).toEqual('1,1,1');
   });
 
-  it('delete items off the list', () => {
+  test('delete items off the list', () => {
     const newStack = new Stack([1, 2, 3, 4]);
     newStack.pop();
     expect(newStack.readableList).toEqual('1,2,3');
   });
 
-  it('return the last item in the list', () => {
+  test('return the last item in the list', () => {
     const newStack = new Stack([1, 2, 3, 4]);
     const peekedItem = newStack.peek();
     expect(peekedItem).toEqual('4');
+  });
+});
+
+describe('CH-02', () => {
+  test('adds item into queue', () => {
+    const newQueue = new Queue();
+    newQueue.enqueue(1);
+    newQueue.enqueue(2);
+    newQueue.enqueue(3);
+    newQueue.enqueue(4);
+    expect(newQueue.readableList).toEqual('1,2,3,4');
+  });
+
+  test('takes out oldest item in the list', () => {
+    const newQueue = new Queue([1, 2, 3, 4]);
+    newQueue.dequeue();
+    expect(newQueue.readableList).toEqual('2,3,4');
+  });
+
+  test('checks if theres another item left in queue', () => {
+    const newQueue = new Queue([1, 2]);
+    expect(newQueue.hasNext()).toEqual(true);
   });
 });
